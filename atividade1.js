df0cc1b857b1b1f964b03752ef37d1bec6698d54
Alysson <99350021+alyssonorwell@users.noreply.github.com>
2022-05-11 13:17:57 -0300

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,10 @@
+function ex(){
+    alert ("VAMOS PARA A CONTINHA");
+    
+    const g = 10;
+    var v0 = document.getElementById ("v0").value;
+
+    tsubida = v0/g;
+    document.getElementById("vem").innerHTML="tempo de subida=" + tsubida;
+    console.log ("tempo de subida=" + tsubida);
+}
